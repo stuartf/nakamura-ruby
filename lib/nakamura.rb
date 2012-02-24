5e8aa06522df0f792c530d30f8323351821fcf9f
Carl Hall <carl@hallwaytech.com>
2012-02-24 13:32:36 -0800

    KERN-2495 Changed log message from warn to info when not sending cookies. Added more details to message.

--- lib/nakamura.rb
+++ lib/nakamura.rb
@@ -176,7 +176,7 @@ module SlingInterface
         end
         set_cookies(req)
       else
-        @log.warn("Path #{uri} is not a Sling URL, not sending state")
+        @log.info("#{uri.host} does not match expected #{@serverui.host}. Not sending state.")
       end
       res = createHttp(uri).start { |http| http.request(req ) }
       save_cookies(res) if (isSlingReq)
